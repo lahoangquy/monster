49cfb638c634e28c15ed9dace2c97a80acce8487
lahoangquy <62043234+lahoangquy@users.noreply.github.com>
2020-09-25 15:13:45 +0930

    Updates

---
+++ precache-manifest.c52803d5898e9ffc9222618436c62a08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b868060245a4a9052870af12101688d3",
+    "url": "/monster/index.html"
+  },
+  {
+    "revision": "2510e3a0020f286dd68a",
+    "url": "/monster/static/css/main.a142fb8a.chunk.css"
+  },
+  {
+    "revision": "78adac64eaa07ff35dea",
+    "url": "/monster/static/js/2.356d906f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster/static/js/2.356d906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2510e3a0020f286dd68a",
+    "url": "/monster/static/js/main.7a5e12b5.chunk.js"
+  },
+  {
+    "revision": "562e33ad7bf5e068a5dc",
+    "url": "/monster/static/js/runtime-main.745f0dd4.js"
+  }
+]);
